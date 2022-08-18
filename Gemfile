cb631a43cf845d92656cae979143b5d71a891be9
Elio Cortes <elio.cortes3000@gmail.com>
2022-08-18 15:41:07 -0500

    Added JWT authorization

--- Gemfile
+++ Gemfile
@@ -6,6 +6,12 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
+gem 'bcrypt', '~> 3.1.7'
+
+ gem 'jwt'
+
+ gem 'simple_command'
+ 
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module CarsAPI
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -23,5 +24,7 @@ module CarsAPI
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    
   end
 end
