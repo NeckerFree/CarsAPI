fda7fcc07390ab39d03520f294e942115514419b
amrendrakind <vision2010@gmail.com>
2022-08-19 23:58:57 +0530

    Update: User model

--- db/migrate/20220819182740_add_column_to_users.rb
+++ db/migrate/20220819182740_add_column_to_users.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-class AddColumnToUser < ActiveRecord::Migration[7.0]
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :age, :integer
     add_column :users, :phone, :string
