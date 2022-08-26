f445e1d49eca41173ed3fece0426b15f900218aa
amrendrakind <vision2010@gmail.com>
2022-08-27 02:53:36 +0530

    Update: change cities controller index

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,8 +3,8 @@ class CitiesController < ApplicationController
 
   # GET /cities
   def index
-    @cities = City.all
-
+    puts params
+    @cities = City.where(country_id: params[:country_id])
     render json: @cities
   end
 
@@ -46,6 +46,6 @@ class CitiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def city_params
-      params.fetch(:city, {})
+      params.fetch(:city, {}).permit(:country_id, :id)
     end
 end
