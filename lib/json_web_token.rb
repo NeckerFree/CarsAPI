d1747c511d2b6f0c3f2288700dad7fa7fa5ffd6e
Elio Cortes <elio.cortes3000@gmail.com>
2022-09-05 15:49:39 -0500

    setup secrets

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,14 +2,15 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.credentials.read)
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.credentials.read)[0]
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
       HashWithIndifferentAccess.new body
-    rescue StandardError
+    rescue JWT::DecodeError
       nil
     end
   end
 end
+
